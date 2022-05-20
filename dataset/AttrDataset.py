af65b82ecac7c425523bb3415522c8892a194a6e
Sehee Kim <seheekim618@gmail.com>
2022-05-21 02:06:30 +0900

    Update AttrDataset.py

--- dataset/AttrDataset.py
+++ dataset/AttrDataset.py
@@ -1,26 +1,18 @@
-import os
-import pickle
-
-import numpy as np
-import torch.utils.data as data
-from PIL import Image
-
 import torchvision.transforms as T
-from pathlib import Path
 
-def get_transform(args):
+def get_transform():
     normalize = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     train_transform = T.Compose([
+        T.ToTensor(),
         T.Resize((256, 192)),
         T.RandomHorizontalFlip(),
-        T.ToTensor(),
         normalize,
     ])
 
     valid_transform = T.Compose([
-        T.Resize((256, 192)),
         T.ToTensor(),
-        normalize
+        T.Resize((256, 192)),
+        normalize,
     ])
 
     return train_transform, valid_transform

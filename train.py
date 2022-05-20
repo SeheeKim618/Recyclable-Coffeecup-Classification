069be04f5ac6130da59c6c3c5d7e55bbfdadc46b
Sehee Kim <seheekim618@gmail.com>
2022-05-21 03:32:06 +0900

    Update train.py

--- train.py
+++ train.py
@@ -21,13 +21,12 @@ warnings.filterwarnings("ignore")
 
 train_tsfm, test_tsfm = get_transform()
 
-data_path = os.path.join("/home/jjunhee98/바탕화면/ksh/data", f"{args.dataset}")
-#data_path = "/home/jjunhee98/바탕화면/ksh/data/cafecup/"
+data_path = os.path.join("./data", f"{args.dataset}")
 
 #get ready for data
-train_set = ImageFolder("/home/jjunhee98/바탕화면/ksh/data/cafecup/train/", train_tsfm)
-val_set = ImageFolder("/home/jjunhee98/바탕화면/ksh/data/cafecup/valid/", train_tsfm)
-test_set = ImageFolder("/home/jjunhee98/바탕화면/ksh/data/cafecup/test/", test_tsfm)
+train_set = ImageFolder("./data/cafecup/train/", train_tsfm)
+val_set = ImageFolder("./data/cafecup/valid/", train_tsfm)
+test_set = ImageFolder("./data/cafecup/test/", test_tsfm)
 
 #load data
 train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, batch_size=args.train_batchsize, num_workers=3)
@@ -58,10 +57,6 @@ epoch = args.train_epoch
 LR_scheduler = lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
 '''
-#model checkpoint
-model_path = './exp_result/checkpoint1'
-file_name = 'checkpoint1test_lr_001_SGD_class4_freeze34-2_new_ver2.pt'
-
 #model load
 if os.path.isdir(checkpoint_path) and os.path.isfile(checkpoint_path + '/' + file_name):
     checkpoint = torch.load(checkpoint_path + '/' + file_name)
@@ -87,29 +82,24 @@ def train(model, criterion, optimizer, scheduler, epochs):
         print('Epoch {}/{}'.format(epoch, epochs - 1))
         print('-' * 10)
 
-        # 각 Epoch은 학습 단계와 검증 단계를 거침
         for phase in ['train', 'valid']:
             if phase == 'train':
-                model.train(True)  # 학습 모드 설정
+                model.train(True)  
             else:
-                model.train(False)  # 검증 모드 설정
+                model.train(False) 
 
             running_loss = 0.0
             running_corrects = 0
 
-            # 데이터 반
             for data in dataloaders[phase]:
-                # 입력 데이터 가져오기
                 inputs, labels = data
 
-                # 데이터를 Vaariable로 만듦
                 if torch.cuda.is_available():
                     inputs = Variable(inputs.cuda())
                     labels = Variable(labels.cuda())
                 else:
                     inputs, labels = Variable(inputs), Variable(labels)
-
-                # 파라미터 기울기 초기화
+                    
                 optimizer.zero_grad()
 
                 # forward
@@ -117,13 +107,11 @@ def train(model, criterion, optimizer, scheduler, epochs):
                 _, preds = torch.max(outputs.data, 1)
                 loss = criterion(outputs, labels)
 
-                # 학습 단계에서만 수행, 역전파 + 옵티마이즈(최적화)
                 if phase == 'train':
                     loss.backward()
                     optimizer.step()
                     scheduler.step()
 
-                # 통계
                 running_loss += loss.data
                 running_corrects += torch.sum(preds == labels.data)
 
@@ -133,7 +121,6 @@ def train(model, criterion, optimizer, scheduler, epochs):
             print('{} |\t Loss: {:.4f}\t Acc: {:.4f}'.format(
                 phase, epoch_loss, epoch_acc))
 
-            # 모델 복사(Deep Copy)
             if phase == 'valid' and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model_wts = model.state_dict()
@@ -145,47 +132,10 @@ def train(model, criterion, optimizer, scheduler, epochs):
                 }
                 save_model(args.model_ckpt, state, args.ckpt_name)
 
-    '''
-    #model test
-    model.train(False)
-
-    running_loss = 0.0
-    running_corrects = 0
-
-    # 데이터 반
-    for data in dataloaders['test']:
-        # 입력 데이터 가져오기
-        inputs, labels = data
-
-        # 데이터를 Vaariable로 만듦
-        if torch.cuda.is_available():
-            inputs = Variable(inputs.cuda())
-            labels = Variable(labels.cuda())
-        else:
-            inputs, labels = Variable(inputs), Variable(labels)
-
-        # 파라미터 기울기 초기화
-        optimizer.zero_grad()
-
-        # forward
-        outputs = model(inputs)
-        _, preds = torch.max(outputs.data, 1)
-        loss = criterion(outputs, labels)
-
-        # 통계
-        running_loss += loss.data
-        running_corrects += torch.sum(preds == labels.data)
-
-    test_loss = running_loss.item() / dataset_sizes['test']
-    test_acc = running_corrects.item() / dataset_sizes['test']
-    '''
-
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(
         time_elapsed // 60, time_elapsed % 60))
     print('Best Valid Accuracy: {:4f}'.format(best_acc))
-    #print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-    #    'test', test_loss, test_acc))
 
     # 최적의 모델 가중치 로딩
     model.load_state_dict(best_model_wts)

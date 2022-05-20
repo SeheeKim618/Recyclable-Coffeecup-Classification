90adb783965fc53ffeec232fafdaf7cb24a10d65
Sehee Kim <seheekim618@gmail.com>
2022-05-21 04:12:06 +0900

    Delete train.sh

--- train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DATASET="cafecup"
-DATASET_MODEL="cafecup"
-MODEL_CKPT="/home/jjunhee98/바탕화면/ksh/exp_result/checkpoint1/"
-CKPT_NAME="checkpoint1test_lr_001_SGD_class4_freeze34-2_new_ver2.pt"
-CUDA_VISIBLE_DEVICES=0 /home/jjunhee98/anaconda3/envs/py38/bin/python train.py $DATASET $DATASET_MODEL --model_ckpt $MODEL_CKPT $CKPT_NAME
